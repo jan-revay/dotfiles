20230d179f14487127e90c6b929289588f4acd95
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-17 21:07:27 +0200

    Config dumps scripts: Encode via 7z just in case

--- .my_scripts/dumps/dumps.sh
+++ .my_scripts/dumps/dumps.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -x
 
-./GNOME_extensions_and_settings_dump.sh &> \
-    ./backup_config_dumps/GNOME_extensions_and_settings_dump_$(date '+%Y%m%d_%H%M%S').log
+readonly GNOME_DUMP_FILENAME="GNOME_extensions_and_settings_dump_$(date '+%Y%m%d_%H%M%S').log"
+# TODO
+#readonly DCONF_DUMP_FILENAME=
+
+pushd backup_config_dumps
+../GNOME_extensions_and_settings_dump.sh &> "${GNOME_DUMP_FILENAME}"
+
+# TODO review (and improve) - encrypt the dumps, just in case
+7z a -p -mx=9 -mhe=on "${GNOME_DUMP_FILENAME}.encrypted" "${GNOME_DUMP_FILENAME}"
+rm "${GNOME_DUMP_FILENAME}"
+
+
+popd
