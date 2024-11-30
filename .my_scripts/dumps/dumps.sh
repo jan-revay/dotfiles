b9a7d766f4d9b9c02be8fded3265d45925373a37
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-12-01 00:01:51 +0100

    dumps.sh: Bugfix - use append instead of overwrite
    Change &> to &>>.

--- .my_scripts/dumps/dumps.sh
+++ .my_scripts/dumps/dumps.sh
@@ -5,8 +5,8 @@ readonly GNOME_DUMP_FILENAME="GNOME_extensions_and_settings_dump_$(date '+%Y%m%d
 #readonly DCONF_DUMP_FILENAME=
 
 pushd backup_config_dumps
-../GNOME_extensions_and_settings_dump.sh &> "${GNOME_DUMP_FILENAME}"
-../print_keybindings.sh &> "${GNOME_DUMP_FILENAME}"
+../GNOME_extensions_and_settings_dump.sh &>> "${GNOME_DUMP_FILENAME}"
+../print_keybindings.sh &>> "${GNOME_DUMP_FILENAME}"
 
 # TODO review (and improve) - encrypt the dumps, just in case
 7z a -p -mx=9 -mhe=on "${GNOME_DUMP_FILENAME}.encrypted" "${GNOME_DUMP_FILENAME}"
