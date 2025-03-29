2e69e1274cf68931795800104bac1d96ab77c421
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-29 05:42:50 +0100

    .my_scripts: toggle_todoist.sh - Version 1

--- .my_scripts/toggle_todoist.sh
+++ .my_scripts/toggle_todoist.sh
@@ -1,7 +1,12 @@
-FOCUS="$(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows.List | head -c -4 | tail -c +3 | jq -c '[.[] | select (.focus == true) | {wm_class: .wm_class}]')"
+#!/bin/bash -x
 
-if [[ "${FOCUS}" == '[{"wm_class":"Todoist"}]' ]]; then
-    exit 0
-fi
+FOCUS="$(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows.List | head -c -4 | tail -c +3 | jq -c '[.[] | select (.focus == true) | {id: .id,wm_class: .wm_class}]')"
+ID_JSON="$(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows.List | head -c -4 | tail -c +3 | jq -c '[.[] | select (.wm_class == "Todoist") | {id: .id}]')"
+ID=$(grep -oP '[0-9]+' <<< "${ID_JSON}")
 
+if [[ "${FOCUS}" =~ '"wm_class":"Todoist"' ]]; then
+    gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows.Minimize "${ID}"
+else
+#     gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows.Unminimize "${ID}"
 todoist
+fi
