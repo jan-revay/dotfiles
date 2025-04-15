a1aa7b28138ff63524eacc9f1dc6502b0e8a880c
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-04-15 23:36:16 +0200

    Dumps: Add `dconf dump /`

--- .my_scripts/dumps/print_keybindings.sh
+++ .my_scripts/dumps/print_keybindings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+# TODO - this is quite incomplete, try searching for modifier keys also
+# e.g. <meta><ctrl>...
+
 echo "==== START - List of all keybindings ===="
 gsettings list-recursively | grep -i -E 'media-keys|keybindings'
+dconf dump / | grep -i -E 'media-keys|keybinding|shortcut'
 echo "==== END - List of all keybindings ===="
