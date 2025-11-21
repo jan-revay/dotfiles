7aec08061daaee5d5e2e3070e74ae2f09b7df596
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-11-21 18:41:31 +0100

    position_windows.sh: Positioning script stub
    - Move Firefox to desktop 1
    - Close Todoist
    - generic solution draft in comment

--- .my_scripts/position_windows.sh
+++ .my_scripts/position_windows.sh
@@ -1,40 +1,73 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# TODO - what if there is more than one instance of the window?
+
+for i in {1..15}; do
+    FIREFOX_IDS=( $(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows \
+        --method org.gnome.Shell.Extensions.Windows.List | rg -o "\[[^\]]+\]" \
+            | jq -c '.[] | select (.wm_class == "firefox_firefox") | .id') )
+    TODOIST_IDS=( $(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows \
+        --method org.gnome.Shell.Extensions.Windows.List | rg -o "\[[^\]]+\]" \
+            | jq -c '.[] | select (.wm_class == "Todoist") | .id') )
+
+    echo "${FIREFOX_IDS[@]}"
+    echo "${TODOIST_IDS[@]}"
+
+    if [[ ${FIREFOX_IDS[0]} != "" && ${TODOIST_IDS[0]} != "" ]]; then
+        break
+    fi
+    
+    sleep 1
+done
+
+gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows \
+    --method org.gnome.Shell.Extensions.Windows.MoveToWorkspace "${FIREFOX_IDS[0]}" 0
+gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows \
+    --method org.gnome.Shell.Extensions.Windows.Close "${TODOIST_IDS[0]}"
+
+# TODO: rewrite using list of windows, map and for_each function
+# napady na faktorizaciu
+# 1. napisat to v pythone? - netreba
+# 2. fcia getIdFromClass
+# 3. fcia get ids (caka v cykle kym vsetky idcka bidi existovat) - vracoa map - nazov triedy -> id
+# 4. samotne cally co roboa s oknami veci
+#
+# # TOREAD https://en.wikipedia.org/wiki/D-Bus
+#
+# windows=(a b c)
+#
+# getWindowIdFromClass(){ # array parameter and map return value, so that I don't need to call DBus multiple times
+#     # dbus call
+#     # returns map with map[loaded] = false so that I can easily check that all ids were found (that one key is enough)
+# }
+#
+# waitForIds(){
+#     WOMDOWS=$1
+#     declare -A IDS
+#
+#     for i in {1..30}; do
+#         for win in $WINDOWS; do
+#             id=getWondowIdFromClass $wim
+#
+#             if [[ $id == "" ]]; then
+#                 sleep 1
+#                 break
+#             fi
+#
+#             IDS[$win]=$is
+#         done
+#     done
+#
+#     return $IDS
+# }
+#
+# TODO maybe use not "BASH" but BASH - find out differences between quoted and unquoted delimiter
+# for_each "gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows." <<"BASH"
+#   MoveToWorkspace "${IDS["Firefox"]}" 0
+#   Close "${IDS["Todoist"]}"
+# BASH
 
 
-napady na faktorizaciu
-1. napisat to v pythone? - netreba
-2. fcia getIdFromClass
-3. fcia get ids (caka v cykle kym vsetky idcka bidi existovat) - vracoa map - nazov triedy -> id
-4. samotne cally co roboa s oknami veci
-
-# TOREAD https://en.wikipedia.org/wiki/D-Bus
-
-windows=(a b c)
-
-getWindowIdFromClass(){ # array parameter and map return value, so that I don't need to call DBus multiple times
-    # dbus call
-    # returns map with map[loaded] = false so that I can easily check that all ids were found (that one key is enough)
-}
-
-waitForIds(){
-    WOMDOWS=$1
-    declare -A IDS
-
-    for i in {1..30}; do
-        for win in $WINDOWS; do
-            id=getWondowIdFromClass $wim
-
-            if [[ $id == "" ]]; then
-                sleep 1
-                break
-            fi
-
-            IDS[$win]=$is
-        done
-    done
-
-    return $IDS
-}
 
 
 
@@ -48,34 +81,34 @@ waitForIds(){
 # sleep 120
 
 # use `$ xprop` to find out the properties of the window (e.g. class)
-
-eval "$(xdotool search --onlyvisible --desktop 3 --shell --class org.gnome.Nautilus)"
-xdotool windowsize "${WINDOWS[1]}" 50% 40%
-xdotool windowsize "${WINDOWS[0]}" 50% 40%
-
-xdotool windowmove "${WINDOWS[0]}" 0% 60%
-xdotool windowmove "${WINDOWS[1]}" 50% 60%
-
-eval "$(xdotool search --onlyvisible --shell --name "Google Chrome")"
-xdotool set_desktop_for_window "${WINDOWS[0]}" 0
-xdotool windowmove "${WINDOWS[0]}" 0% 16%
-xdotool windowsize "${WINDOWS[0]}" 102% 86%
-
-eval "$(xdotool search --onlyvisible --shell --name "Mozilla Firefox")"
-xdotool set_desktop_for_window "${WINDOWS[0]}" 2
-xdotool windowmove "${WINDOWS[0]}" 0% 16%
-xdotool windowsize "${WINDOWS[0]}" 102% 86%
-
-eval "$(xdotool search --onlyvisible --shell --name "Todoist")"
-xdotool set_desktop_for_window "${WINDOWS[0]}" 1
-xdotool windowmove "${WINDOWS[0]}" 0% 0%
-xdotool windowsize "${WINDOWS[0]}" 80% 45%
-
-eval "$(xdotool search --onlyvisible --shell --name "Google Keep")"
-xdotool set_desktop_for_window "${WINDOWS[0]}" 2
-
-eval "$(xdotool search --onlyvisible --shell --name "Caprine")"
-xdotool set_desktop_for_window "${WINDOWS[0]}" 4
-
-eval "$(xdotool search --onlyvisible --shell --name "Signal")"
-xdotool set_desktop_for_window "${WINDOWS[0]}" 4
+#
+# eval "$(xdotool search --onlyvisible --desktop 3 --shell --class org.gnome.Nautilus)"
+# xdotool windowsize "${WINDOWS[1]}" 50% 40%
+# xdotool windowsize "${WINDOWS[0]}" 50% 40%
+#
+# xdotool windowmove "${WINDOWS[0]}" 0% 60%
+# xdotool windowmove "${WINDOWS[1]}" 50% 60%
+#
+# eval "$(xdotool search --onlyvisible --shell --name "Google Chrome")"
+# xdotool set_desktop_for_window "${WINDOWS[0]}" 0
+# xdotool windowmove "${WINDOWS[0]}" 0% 16%
+# xdotool windowsize "${WINDOWS[0]}" 102% 86%
+#
+# eval "$(xdotool search --onlyvisible --shell --name "Mozilla Firefox")"
+# xdotool set_desktop_for_window "${WINDOWS[0]}" 2
+# xdotool windowmove "${WINDOWS[0]}" 0% 16%
+# xdotool windowsize "${WINDOWS[0]}" 102% 86%
+#
+# eval "$(xdotool search --onlyvisible --shell --name "Todoist")"
+# xdotool set_desktop_for_window "${WINDOWS[0]}" 1
+# xdotool windowmove "${WINDOWS[0]}" 0% 0%
+# xdotool windowsize "${WINDOWS[0]}" 80% 45%
+#
+# eval "$(xdotool search --onlyvisible --shell --name "Google Keep")"
+# xdotool set_desktop_for_window "${WINDOWS[0]}" 2
+#
+# eval "$(xdotool search --onlyvisible --shell --name "Caprine")"
+# xdotool set_desktop_for_window "${WINDOWS[0]}" 4
+#
+# eval "$(xdotool search --onlyvisible --shell --name "Signal")"
+# xdotool set_desktop_for_window "${WINDOWS[0]}" 4
