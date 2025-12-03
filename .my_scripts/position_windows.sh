3d0be29b8c9a7f64647f68b934f4b5657e8f7549
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-12-03 20:55:42 +0100

    position_windows.sh: Delay 0.05 -> 0.15 on Desktop 1

--- .my_scripts/position_windows.sh
+++ .my_scripts/position_windows.sh
@@ -153,30 +153,6 @@ ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
 sleep 0.05
 ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
 
-ydotool key ${KEY_LEFTALT}:1 ${KEY_1}:1 ${KEY_1}:0 ${KEY_LEFTALT}:0
-sleep 0.05
-ydotool key ${KEY_LEFTALT}:1 ${KEY_LEFTSHIFT}:1 ${KEY_Q}:1 \
-    ${KEY_Q}:0 ${KEY_LEFTALT}:0 ${KEY_LEFTSHIFT}:0
-sleep 0.05
-# Send more KEY_ENTER events just in case there are more windows open
-# on desktop 1
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-sleep 0.05
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-sleep 0.05
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-sleep 0.05
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-sleep 0.05
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-sleep 0.05
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-sleep 0.05
-ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
-# for some reason, longer delay is needed here...
-# otherwise the next shortcut does not work
-sleep 0.25
-
 ydotool key ${KEY_LEFTALT}:1 ${KEY_8}:1 ${KEY_8}:0 ${KEY_LEFTALT}:0
 sleep 0.05
 ydotool key ${KEY_LEFTALT}:1 ${KEY_LEFTSHIFT}:1 ${KEY_Q}:1 \
@@ -221,6 +197,31 @@ sleep 0.05
 ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
 sleep 0.05
 ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+
+sleep 3
+ydotool key ${KEY_LEFTALT}:1 ${KEY_1}:1 ${KEY_1}:0 ${KEY_LEFTALT}:0
+sleep 0.15
+ydotool key ${KEY_LEFTALT}:1 ${KEY_LEFTSHIFT}:1 ${KEY_Q}:1 \
+    ${KEY_Q}:0 ${KEY_LEFTALT}:0 ${KEY_LEFTSHIFT}:0
+sleep 0.15
+# Send more KEY_ENTER events just in case there are more windows open
+# on desktop 1
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+sleep 0.15
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+sleep 0.15
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+sleep 0.15
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+sleep 0.15
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+sleep 0.15
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+sleep 0.15
+ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
+# for some reason, longer delay is needed here...
+# otherwise the next shortcut does not work
+sleep 0.25
 
 # TODO: rewrite using list of windows, map and for_each function
 # napady na faktorizaciu
