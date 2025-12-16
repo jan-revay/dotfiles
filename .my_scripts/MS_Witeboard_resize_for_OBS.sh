066c72d006737cf1b1eb66d3aec12a8515582331
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-12-16 17:21:35 +0100

    MS_Witeboard_resize_for_OBS.sh: First draft

---
+++ .my_scripts/MS_Witeboard_resize_for_OBS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +x
+. ./window_calls_prelude.sh
+
+for i in {1..35}; do
+    WIN_LIST=$(win_list)
+
+    MS_WHITEBOARD_IDS=( $(ids_from_wm_class chrome-fjjhhjjhgaipgagmgnjalbkgadhniebn-Default) )
+    if (( ${#MS_WHITEBOARD_IDS[@]} > 0 )); then
+        break
+    fi
+    
+    sleep 1
+done
+
+
+# MoveResize takes 3 parameters: winid x y width height
+
+win MoveResize "${MS_WHITEBOARD_IDS[0]}" 8 49 3277 2104
