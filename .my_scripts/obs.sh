5e0b84f1f56439ca7ba41e27b7753cc55dd70ceb
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-05-30 18:53:15 +0200

    obs.sh: pw-link audio recorder with no limitter

--- .my_scripts/obs.sh
+++ .my_scripts/obs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-bash -c "__NV_DISABLE_EXPLICIT_SYNC=1 flatpak run com.obsproject.Studio" &
-sleep 15
+bash -c "__NV_DISABLE_EXPLICIT_SYNC=1 flatpak run com.obsproject.Studio --verbose" &
+sleep 7
 
 pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK no effects:in_1"
 pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK effects:in_1"
+pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK no limitter:in_1"
 
 # TODO also try to connect student audio automatically
