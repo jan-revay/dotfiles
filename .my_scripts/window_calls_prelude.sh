a673a327653f16728d18221825b9e49b990814af
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-05-31 12:58:41 +0200

    window_calls_prelude.sh: Add focused_workspace() function

--- .my_scripts/window_calls_prelude.sh
+++ .my_scripts/window_calls_prelude.sh
@@ -27,6 +27,11 @@ id_of_focused() {
     jq -c '.[] | select(.focus) | .id' <<< "${WIN_LIST}"
 }
 
+
+focused_workspace() {
+    jq -c '.[] | select(.focus) | .workspace' <<< "${WIN_LIST}"
+}
+
 win_details_from_id() {
    win Details $1 | sed "s/^('//; s/',)$//" | jq .
 }
