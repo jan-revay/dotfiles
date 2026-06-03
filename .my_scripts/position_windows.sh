3aee7c58944110182101786a43f22869f774524b
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-03 09:17:42 +0200

    position_windows.sh: Remove old X11 part of the script

--- .my_scripts/position_windows.sh
+++ .my_scripts/position_windows.sh
@@ -10,6 +10,8 @@ delay() {
 #   consider doing it by virtual desktop
 # - window positions and behaviors should be defined in a separate configuration file
 # - set up tilingshell to work with this script
+# TODO try writing this as a gnome extension with DSL for wondow
+# positioning
 
 # TODO consider using this trick everywhere
 # resolve the directory where THIS script lives
@@ -298,54 +300,3 @@ ydotool key ${KEY_ENTER}:1 ${KEY_ENTER}:0
 #     return $IDS
 # }
 #
-# TODO maybe use not "BASH" but BASH - find out differences between quoted and unquoted delimiter
-# for_each "gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/Windows --method org.gnome.Shell.Extensions.Windows." <<"BASH"
-#   MoveToWorkspace "${IDS["Firefox"]}" 0
-#   Close "${IDS["Todoist"]}"
-# BASH
-
-
-
-
-
-
-# TODO Wayland support
-# TODO improve - try sth. that is not dependent on timing so much
-# TODO remove dependence on the put window GNOME extension
-# TODO try writing my own GNOME extension for this stuff
-# TODO ?try to solve this via virtual desktop-specific tiling?
-
-# sleep 120
-
-# use `$ xprop` to find out the properties of the window (e.g. class)
-#
-# eval "$(xdotool search --onlyvisible --desktop 3 --shell --class org.gnome.Nautilus)"
-# xdotool windowsize "${WINDOWS[1]}" 50% 40%
-# xdotool windowsize "${WINDOWS[0]}" 50% 40%
-#
-# xdotool windowmove "${WINDOWS[0]}" 0% 60%
-# xdotool windowmove "${WINDOWS[1]}" 50% 60%
-#
-# eval "$(xdotool search --onlyvisible --shell --name "Google Chrome")"
-# xdotool set_desktop_for_window "${WINDOWS[0]}" 0
-# xdotool windowmove "${WINDOWS[0]}" 0% 16%
-# xdotool windowsize "${WINDOWS[0]}" 102% 86%
-#
-# eval "$(xdotool search --onlyvisible --shell --name "Mozilla Firefox")"
-# xdotool set_desktop_for_window "${WINDOWS[0]}" 2
-# xdotool windowmove "${WINDOWS[0]}" 0% 16%
-# xdotool windowsize "${WINDOWS[0]}" 102% 86%
-#
-# eval "$(xdotool search --onlyvisible --shell --name "Todoist")"
-# xdotool set_desktop_for_window "${WINDOWS[0]}" 1
-# xdotool windowmove "${WINDOWS[0]}" 0% 0%
-# xdotool windowsize "${WINDOWS[0]}" 80% 45%
-#
-# eval "$(xdotool search --onlyvisible --shell --name "Google Keep")"
-# xdotool set_desktop_for_window "${WINDOWS[0]}" 2
-#
-# eval "$(xdotool search --onlyvisible --shell --name "Caprine")"
-# xdotool set_desktop_for_window "${WINDOWS[0]}" 4
-#
-# eval "$(xdotool search --onlyvisible --shell --name "Signal")"
-# xdotool set_desktop_for_window "${WINDOWS[0]}" 4
