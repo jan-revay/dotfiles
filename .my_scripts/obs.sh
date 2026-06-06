e7e0ba6ebf3be8f65331f6b55b364f4e82f72d72
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-06 18:59:17 +0200

    obs.sh: Use virtual sink for Discord

--- .my_scripts/obs.sh
+++ .my_scripts/obs.sh
@@ -7,10 +7,10 @@ sleep 7
 pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK no effects:in_1"
 pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK effects:in_1"
 pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK no limiter:in_1"
-# TODO do this via a virtual sink that is connected to default sink and set in
-# discord as an output device
-pw-link "alsa_output.usb-Focusrite_Scarlett_2i2_4th_Gen_S2JYTQ63508147-00.pro-output-0:monitor_AUX0" \
-        "OBS Studio: Student JCK:in_1"
+# NOTE: This records all desktop audio output
+# pw-link "alsa_output.usb-Focusrite_Scarlett_2i2_4th_Gen_S2JYTQ63508147-00.pro-output-0:monitor_AUX0" \
+        # "OBS Studio: Student JCK:in_1"
+pw-link "Discord_virtual_sink:monitor_FL" "OBS Studio: Student JCK:in_1"
 
 # TODO also try to connect student audio automatically
 
