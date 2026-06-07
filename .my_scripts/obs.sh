f68d28e0c2a3466e51843a272c6da8a8216d494a
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-07 13:39:23 +0200

    obs.sh: Use workspace 13 for sharing entire screen
    - add one workspace
    - move workspaces one right
    - add an alert and logging if I am sharing screen on a workspace other
      than 13

--- .my_scripts/obs.sh
+++ .my_scripts/obs.sh
@@ -42,29 +42,38 @@ pw-link "Discord_virtual_sink:monitor_FL" "OBS Studio: Student JCK:in_1"
 # TODO also try to connect student audio automatically
 
 obs-cmd fullscreen-projector
-
-# resolve the directory where THIS script lives
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-. "${SCRIPT_DIR}/window_calls_prelude.sh"
+sleep 1
 
 PROJECTOR_ID=$(win_list | jq '.[] | select(.wm_class == "com.obsproject.Studio") | select(.title == "Projector - Program") | .id')
-win MoveToWorkspace "${PROJECTOR_ID}" 16
+win MoveToWorkspace "${PROJECTOR_ID}" 17
 
 readonly SCENE1="1_my_whiteboard"
 readonly SCENE2="2_student_screen"
 readonly SCENE3="3_my_screen"
+
+sleep 2
+obs-cmd scene switch "${SCENE1}"
 
 while true; do
     WORKSPACE=$(wmctrl -d | grep '\*' | awk '{print $1}')
     SCENE_STR=$(obs-cmd scene current)
     CURRENT_SCENE=${SCENE_STR##*Current scene: }
 
-    if [[ "${WORKSPACE}" == "13" && "${CURRENT_SCENE}" != "${SCENE1}" ]]; then
+    if [[ "${WORKSPACE}" == "14" && "${CURRENT_SCENE}" != "${SCENE1}" ]]; then
         obs-cmd scene switch "${SCENE1}"
-    elif [[ "${WORKSPACE}" == "14" && "${CURRENT_SCENE}" != "${SCENE2}" ]]; then
+    elif [[ "${WORKSPACE}" == "15" && "${CURRENT_SCENE}" != "${SCENE2}" ]]; then
         obs-cmd scene switch "${SCENE2}"
-    elif [[ "${WORKSPACE}" == "15" && "${CURRENT_SCENE}" != "${SCENE3}" ]]; then
+    elif [[ "${WORKSPACE}" == "16" && "${CURRENT_SCENE}" != "${SCENE3}" ]]; then
         obs-cmd scene switch "${SCENE3}"
+    fi
+
+    if [[ "${WORKSPACE}" -lt "13" && "${CURRENT_SCENE}" == "ENTIRE_SCREEN" ]]; then
+        # I have set up a keybinding to switch to this scene, but I want to
+        # only allow it on workspace 13, so that I don't stream my private msgs
+        # and stuff an by accident.
+        pw-play ~/Music/Napalm_Death_You_Suffer.flac
+        echo "$(date) OBS: Sharing entire screen on workspace ${WORKSPACE}" \
+            >> ~/tmp/obs_screenshare.log
     fi
 
     sleep 0.4
