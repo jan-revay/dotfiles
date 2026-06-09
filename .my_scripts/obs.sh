fa7079729c755aa9582abb9a7caed23e62f47c80
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-09 22:44:22 +0200

    obs.sh: Tighten sleep during startup

--- .my_scripts/obs.sh
+++ .my_scripts/obs.sh
@@ -5,30 +5,30 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 . "${SCRIPT_DIR}/window_calls_prelude.sh"
 
 ./WB.sh &
-sleep 3
+sleep 2
 
 wmctrl -s 16
 kitty --title "Tutoring" &
 firefox --private-window "https://theproductivedeveloper.com/" &
 
-sleep 4
+sleep 2
 
 kitty=$(win_list | jq -c '.[] | select(.title == "Tutoring") | .id')
 firefox=$(win_list | jq -c '.[] | select(.title == "Hyperspace by HTML5 UP — Mozilla Firefox Private Browsing") | .id')
 
 win Activate "${kitty[0]}"
-sleep 0.1
+sleep 0.03
 ydotool key ${KEY_LEFTALT}:1 ${KEY_Z}:1 ${KEY_Z}:0 ${KEY_LEFTALT}:0
-sleep 0.1
+sleep 0.03
 win Activate "${firefox[0]}"
-sleep 0.1
+sleep 0.03
 ydotool key ${KEY_LEFTALT}:1 ${KEY_X}:1 ${KEY_X}:0 ${KEY_LEFTALT}:0
-sleep 1
+sleep 0.4
 
 wmctrl -s 2
 
 bash -c "__NV_DISABLE_EXPLICIT_SYNC=1 flatpak run com.obsproject.Studio --verbose" &
-sleep 7
+sleep 6
 
 # TODO limiter is spelled with a single T
 pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK no effects:in_1"
@@ -39,10 +39,8 @@ pw-link "My 2i2_MONO:capture_MONO" "OBS Studio: sm7 JCK no limiter:in_1"
         # "OBS Studio: Student JCK:in_1"
 pw-link "Discord_virtual_sink:monitor_FL" "OBS Studio: Student JCK:in_1"
 
-# TODO also try to connect student audio automatically
-
 obs-cmd fullscreen-projector
-sleep 1
+sleep 0.7
 
 PROJECTOR_ID=$(win_list | jq '.[] | select(.wm_class == "com.obsproject.Studio") | select(.title == "Projector - Program") | .id')
 win MoveToWorkspace "${PROJECTOR_ID}" 17
@@ -51,7 +49,6 @@ readonly SCENE1="1_my_whiteboard"
 readonly SCENE2="2_student_screen"
 readonly SCENE3="3_my_screen"
 
-sleep 2
 obs-cmd scene switch "${SCENE1}"
 
 while true; do
