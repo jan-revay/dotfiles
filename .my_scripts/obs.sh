f171fa66071f6b3bb5f8beac329a88d0d3e252cc
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-28 14:28:03 +0200

    obs.sh: Add a TODO

--- .my_scripts/obs.sh
+++ .my_scripts/obs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
-#TODO try whether disabling pro audio mode fixes the obs buffering issues
+# TODO try whether disabling pro audio mode fixes the obs and Discord buffering
+# issues
+# TODO measure latency woth pro audio mode on vs. off
 
 apps() {
 
